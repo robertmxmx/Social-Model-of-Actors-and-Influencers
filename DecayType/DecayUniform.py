784eef9c4fd445ffd5bcc817bccb755004cae07d
robertmxmx <70693574+robertmxmx@users.noreply.github.com>
2021-11-23 18:22:17 +1100

    Update DecayUniform.py

--- DecayType/DecayUniform.py
+++ DecayType/DecayUniform.py
@@ -2,7 +2,7 @@ def decay(Time_Since_Last_Interaction,Opinion_List,random,chance,amount):
     #applies on each relationship, a "chance" percent of the time move the relationship closer to 0 by "amount"
     for i in range(len(Opinion_List)):
         for j in range(i + 1, len(Opinion_List[0])):
-            if random.next() <= chance and abs(Opinion_List[i][j]) >= amount:
+            if random.next() < chance and abs(Opinion_List[i][j]) > amount:
                 if Opinion_List[i][j] > 0:
                     Opinion_List[i][j] -= amount
                 elif Opinion_List[i][j] < 0:
